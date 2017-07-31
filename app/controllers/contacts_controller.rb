87098fbdf8ee388ba1087b8507ce4c3e2fac18f8
John Boyle <jwsboyle@gmail.com>
2017-07-31 10:01:51 +0000

    Fixed contact db

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,19 @@
 class ContactsController < ApplicationController
   def new
+    @contact = Contact.new #fires the contact and stores in @contact
   end
+  
+  def Create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to new_contact_path, notice: "Message sent."
+    else
+      redirect_to new_contact_path, notice: "Error occured."
+    end
+  end
+  
+  private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
+    end
 end
